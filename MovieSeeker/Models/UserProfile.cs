a373cee1dca590e43ab518c5c8d2ebe19260a678
Rukshan Dangalla <rukshandangalla@gmail.com>
2017-06-10 14:03:51 +0530

    adding new user profile object

---
+++ MovieSeeker/Models/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovieSeeker.Models
+{
+    [Serializable]
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+    }
+}
